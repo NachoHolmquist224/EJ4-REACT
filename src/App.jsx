f9b5a096cab26cb25d2249f64711b20ea5e36a66
Ignacio Holmquist <ignacioholmquist12@gmail.com>
2026-04-29 18:24:39 -0300

    Agg el footer y el nav a App y tmb agg css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <>
-      <h1>React App</h1>
+      <Navbar></Navbar>
+      <main></main>
+      <Footer></Footer>
     </>
   )
 }
